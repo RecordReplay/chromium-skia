5ee8377bc6829793acff2c25fcb65017002f6aa4
Brian Hackett <bhackett1024@gmail.com>
2021-04-29 05:53:45 -0600

    Add diagnostic for backend issue 1819

--- src/core/SkVMBlitter.cpp
+++ src/core/SkVMBlitter.cpp
@@ -22,6 +22,25 @@
 
 #include <cinttypes>
 
+#include <dlfcn.h>
+
+static void (*gRecordReplayAssertFn)(const char*, va_list);
+
+static void RecordReplayAssert(const char* aFormat, ...) {
+  if (!gRecordReplayAssertFn) {
+    void* fnptr = dlsym(RTLD_DEFAULT, "RecordReplayAssert");
+    if (!fnptr) {
+      return;
+    }
+    gRecordReplayAssertFn = reinterpret_cast<void(*)(const char*, va_list)>(fnptr);
+  }
+
+  va_list ap;
+  va_start(ap, aFormat);
+  gRecordReplayAssertFn(aFormat, ap);
+  va_end(ap);
+}
+
 namespace {
 
     // Uniforms set by the Blitter itself,
@@ -554,6 +573,7 @@ namespace {
         {}
 
         ~Blitter() override {
+            RecordReplayAssert("Blitter::~Blitter");
             if (SkLRUCache<Key, skvm::Program>* cache = try_acquire_program_cache()) {
                 auto cache_program = [&](skvm::Program&& program, Coverage coverage) {
                     if (!program.empty()) {
