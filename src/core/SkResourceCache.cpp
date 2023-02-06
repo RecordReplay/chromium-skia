af2c9dc556b3415cce18d83aa1cc4b2cb931a095
Domi <Domiii@users.noreply.github.com>
2023-02-06 22:10:48 +0800

    Assert SkResourceCache

--- src/core/SkResourceCache.cpp
+++ src/core/SkResourceCache.cpp
@@ -119,7 +119,9 @@ SkResourceCache::~SkResourceCache() {
 bool SkResourceCache::find(const Key& key, FindVisitor visitor, void* context) {
     this->checkMessages();
 
-    if (auto found = fHash->find(key)) {
+    auto found = fHash->find(key);
+    SkRecordReplayAssert("[RUN-593] SkResourceCache::find %d", found);
+    if (found) {
         Rec* rec = *found;
         if (visitor(*rec, context)) {
             this->moveToHead(rec);  // for our LRU
@@ -166,6 +168,10 @@ void SkResourceCache::add(Rec* rec, void* payload) {
     fHash->set(rec);
     rec->postAddInstall(payload);
 
+    // https://linear.app/replay/issue/RUN-593
+    SkRecordReplayAssert(
+        "[RUN-593] SkResourceCache::add %d %s", rec->bytesUsed(), rec->getCategory());
+
     if (gDumpCacheTransactions) {
         SkString bytesStr, totalStr;
         make_size_str(rec->bytesUsed(), &bytesStr);
@@ -182,6 +188,10 @@ void SkResourceCache::remove(Rec* rec) {
     SkASSERT(rec->canBePurged());
     size_t used = rec->bytesUsed();
     SkASSERT(used <= fTotalBytesUsed);
+
+    // https://linear.app/replay/issue/RUN-593
+    SkRecordReplayAssert(
+            "[RUN-593] SkResourceCache::remove %d %s", rec->bytesUsed(), rec->getCategory());
 
     this->release(rec);
     fHash->remove(rec->getKey());
